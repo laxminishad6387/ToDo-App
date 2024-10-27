6e94f955245fc0cf9afada3a20025d3eaf61ac68
laxminishad88 <laxminishad00000@gmail.com>
2024-10-27 21:06:21 +0530

    handle all button

--- script.js
+++ script.js
@@ -7,35 +7,79 @@ function localstorage()
      return todos
 
 }
-function addtodo(inputText)
+function addtodo(todo)
 {
       const newtodo=localstorage();
-       newtodo.todoList.push(inputText);
+       newtodo.todoList.push(todo);
        localStorage.setItem("todo",JSON.stringify(newtodo));
 }
-function addToDoInhtml(todoText)
+function  findFilterButton(event)
+{      const todoList=document.getElementById("todolist"); 
+      const element=event.target;
+      const value=element.getAttribute("data-filter");
+      console.log(value);
+      todoList.innerHTML='';
+      const todos= localstorage();
+      if(value=="All")
+      {
+          
+     todos.todoList.forEach(todo=>
+     {
+      addToDoInhtml(todo);
+     }
+     )
+      } 
+      else  if(value=="Pending")
+      {   
+
+            todos.todoList.forEach(todo=>
+                  {  
+                        if(todo.isCompleted!=true)
+                   addToDoInhtml(todo);
+                  }
+                  )
+      }
+      else{
+            todos.todoList.forEach(todo=>
+                  {  
+                        if(todo.isCompleted==true)
+                   addToDoInhtml(todo);
+                  }
+                  )
+      }
+     
+}
+function addToDoInhtml(todo)
 {
-      const newTodoHTML=document.getElementById("todolist");
-      const newTodoElement=document.createElement("li");
-      newTodoElement.textContent=todoText;
-      newTodoElement.classList.add("addItem");
+      const ulElement=document.getElementById("todolist");
+      const liElement=document.createElement("li");
+      const textdiv=document.createElement("div");
+      const  wrapper=document.createElement("div");
+      
+      
+      textdiv.textContent=todo.text;
+      liElement.classList.add("addItem");
 
     const editbtn=document.createElement("button");
     editbtn.classList.add("editbtn");
     editbtn.textContent="Edit";
-    newTodoElement.appendChild(editbtn);
+
 
     const deletebtn=document.createElement("button");
     deletebtn.textContent="Delete";
     deletebtn.classList.add("deletebtn");
-    newTodoElement.appendChild(deletebtn);
+    
 
     const completedbtn=document.createElement("button");
     completedbtn.textContent="Completed";
     completedbtn.classList.add("completedbtn");
-    newTodoElement.appendChild(completedbtn);
+wrapper.appendChild(editbtn);
+    wrapper.appendChild(deletebtn);
+  wrapper.appendChild(completedbtn);
 
-      newTodoHTML.appendChild(newTodoElement);
+  liElement.appendChild(textdiv);
+    liElement.appendChild(wrapper);
+  ulElement.appendChild(liElement);
 }
 
 
@@ -43,6 +87,11 @@ document.addEventListener("DOMContentLoaded",()=>
 {
     const inputText=document.getElementById("InputType");
     const submited=document.getElementById("ToDoType");
+    const filterbtn=document.getElementsByClassName("filterbtn");
+    for(const btn of filterbtn)
+    {
+        btn.addEventListener("click",findFilterButton);
+    }
     submited.addEventListener("click",()=>
 {    
       const todoText=inputText.value;
@@ -50,8 +99,8 @@ document.addEventListener("DOMContentLoaded",()=>
             alert("please add something in todo");
       else
       {
-           addtodo(todoText);
-      addToDoInhtml(todoText);
+           addtodo({text:todoText , isCompleted:false});
+      addToDoInhtml({text:todoText , isCompleted:false});
       inputText.value="";
       }
 })
@@ -64,10 +113,7 @@ document.addEventListener("DOMContentLoaded",()=>
      const todos= localstorage();
      todos.todoList.forEach(todo=>
      {
-      const newTodoHTML=document.getElementById("todolist");
-      const newTodoElement=document.createElement("li");
-      newTodoElement.textContent=todo;
-      newTodoHTML.appendChild(newTodoElement);
+      addToDoInhtml(todo);
      }
      )
 });
